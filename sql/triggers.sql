4cb6835db821748139bd1667945d4e7d9153e911
Jeremy D. Miller <jeremydmiller@yahoo.com>
2014-05-30 11:54:34 -0500

    little cleanup and calling this a successful spike

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,7 +1,3 @@
-DROP TABLE IF EXISTS TRACE CASCADE;
-CREATE TABLE TRACE (
-	text	varchar(250)
-);
 
 DROP TABLE IF EXISTS Party_Projection CASCADE;
 CREATE TABLE Party_Projection (
@@ -10,11 +6,41 @@ CREATE TABLE Party_Projection (
 );
 
 CREATE OR REPLACE FUNCTION update_Party_projection() RETURNS trigger AS $$
+	var projection = {
+		init: function(){
+			return {};
+		},
+
+		QuestStarted: function(state, evt){
+			state.Started = true;
+		}
+	};
+
+	var transform = projection[NEW.type];
+	var id = NEW.stream_id;
+	var raw = plv8.execute('SELECT data FROM Party_Projection WHERE id = $1', [id]);
+
+	var plan = null;
+	var data = null;
+	if (raw.length == 1){
+		if (!transform){
+			return;
+		}
+
+		data = raw[0];
+		plan = plv8.prepare('UPDATE Party_Projection SET data = $2 where id = $1');
+	}
+	else {
+		data = projection.init();
+		plan = plv8.prepare('INSERT INTO Party_Projection (id, data) values ($1, $2)');
+	}
+
 	
+	if (transform){
+		transform(data, NEW.data);
+	}
 
-	//var plan = plv8.prepare( 'INSERT INTO Party_Projection (id, data) values ($1)', ['json'] );
-
-	//plan.execute([transform(NEW.data)]);
+	plan.execute([id, data]);
 $$ LANGUAGE plv8;
 
 DROP TRIGGER update_Party_projection ON events CASCADE;
@@ -22,45 +48,27 @@ CREATE TRIGGER update_Party_projection AFTER INSERT ON events
     FOR EACH ROW EXECUTE PROCEDURE update_Party_projection();
 
 
---CREATE OR REPLACE FUNCTION json_play(left JSON, right JSON)
---RETURNS JSON AS $$
---  for (var key in right) { left[key] = right[key]; }
---  return left;
---$$ LANGUAGE plv8;
-
---select json_play('{"a":"1", "b":"2"}', '{"c":"3"}');
 
 truncate streams cascade;
+truncate Party_Projection cascade;
+
+SELECT load_stream('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', '
+	{
+		"type":"Quest",
+		"events":[
+			{"type":"QuestStarted", "data":{"location": "Emond''s Field", "members":["Rand", "Perrin", "Mat", "Lan"]}},
+			{"type":"TownReached", "data":{"location": "Taren Ferry"}},
+			{"type":"MemberLost", "data":{"member": "Thom"}},
+			{"type":"MemberJoined", "data":{"member": "Nynaeve"}},
+			{"type":"TownReached", "data":{"location": "Baerlon"}},
+			{"type":"QuestEnded", "data":{"location":"Eye of the World"}}
+		]	
+
+	}
+');
 
 
-select append_event('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', 'quest', 'QuestStarted', 
-	'{"location": "Two Rivers", "members":["Rand", "Perrin", "Mat", "Lan"]}');
+select * from Party_Projection;
 
-select append_event('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', 'quest', 'TownReached', 
-	'{"location": "Taren Ferry"}');
-
-select append_event('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', 'quest', 'MemberLost', 
-	'{"member": "Thom"}');
-
-select append_event('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', 'quest', 'MemberJoined', 
-	'{"member": "Nynaeve"}');
-
-select append_event('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', 'quest', 'TownReached', 
-	'{"location": "Baerlon"}');
-
-select append_event('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', 'quest', 'EnemyDispatched', 
-	'{"location":"Shadar Logoth" "Trollocs": {"Mat":1, "Perrin":"2", "Rand":"3"}, "Fades":{"Lan":1}}');
-
-select append_event('cdd82fef-2c14-46a5-a2f3-e866cc6f4568', 'quest', 'EnemyDispatched', 
-	'{"location":"Caemlyn" "Trollocs": {"Lan":3}, "Fades":{"Lan":1}}');
-
-
-
-
-select * from TRACE;
-
---select * from streams;
-
---select * from events;
 
 
