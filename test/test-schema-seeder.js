73f53a8979842c5997a145edb8691ac19e4adbac
jeremydmiller <jeremydmiller@yahoo.com>
2014-06-10 14:25:35 -0500

    able to load javascript modules into the database now

--- test/test-schema-seeder.js
+++ test/test-schema-seeder.js
@@ -14,41 +14,17 @@ describe('The Database Seeder', function(){
 			.then(function(result){
 
 				expect(result.tables).to.deep.equal(['pge_events', 'pge_modules', 'pge_projection_definitions', 'pge_projections', 'pge_streams']);
-
+				
+				expect(result.modules).to.deep.equal(['EventStore']);
 
 				done();
 			})
 			.error(function(err){
 				done(err);
 			});
-		});
+	});
+
 });
 
-/*
 
-
-	it('should insert the EventStore module', function(){
-console.log('I am in here.');
-		require('pg').connect(connection, function(err, client, done){
-			expect(err).to.be.null;
-console.log('I am in here.');
-			client.query("select name from pge_modules", function(err, result){
-				console.log('I am in here.');
-
-				expect(err).to.be.null;
-
-				expect(result.rows.length).to.equal(1);
-
-				for (var i = 0; i < result.rows.length; i++){
-					console.log(result[i].name);
-				}
-
-				expect(result.rows[0].name).to.equal('EventStore');
-
-				done(client);
-			});
-
-		});
-	});
-*/
 
