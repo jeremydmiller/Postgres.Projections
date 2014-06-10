3d00850f508fd9d46a7ff6243bb0d821fc5f2b44
jeremydmiller <jeremydmiller@yahoo.com>
2014-06-10 14:17:24 -0500

    starting to generalize the database loading

--- test/test-schema-seeder.js
+++ test/test-schema-seeder.js
@@ -12,47 +12,20 @@ describe('The Database Seeder', function(){
 	it('Should be able to seed the basic database schema', function(done){
 		seeder.seedAll({connection: connection})
 			.then(function(result){
-				expect(result.rowCount).to.equal(5);
 
-				expect(result.rows[0].table_name).to.equal('pge_events');
-				expect(result.rows[1].table_name).to.equal('pge_modules');
-				expect(result.rows[2].table_name).to.equal('pge_projection_definitions');
-				expect(result.rows[3].table_name).to.equal('pge_projections');
-				expect(result.rows[4].table_name).to.equal('pge_streams');
+				expect(result.tables).to.deep.equal(['pge_events', 'pge_modules', 'pge_projection_definitions', 'pge_projections', 'pge_streams']);
+
 
 				done();
 			})
 			.error(function(err){
 				done(err);
 			});
-
-	});
+		});
+});
 
 /*
-	it.only('Should be able to seed the basic database schema', function(){
-		var pg = require('pg');
-		pg.connect(connection, function(err, client, done){
-			expect(err).to.be.null;
 
-			client.query("select table_name from information_schema.tables where table_name like 'pge_%' order by table_name", function(err, result){
-				expect(err).to.be.null;
-				expect(result.rows.length).to.equal(5);
-				
-				expect(result.rows[0].table_name).to.equal('pge_events');
-				expect(result.rows[1].table_name).to.equal('pge_modules');
-				expect(result.rows[2].table_name).to.equal('pge_projection_definitions');
-				expect(result.rows[3].table_name).to.equal('pge_projections');
-				expect(result.rows[4].table_name).to.equal('pge_streams');
-				done(client);
-			});
-
-
-
-		});
-
-		
-
-	});
 
 	it('should insert the EventStore module', function(){
 console.log('I am in here.');
@@ -79,4 +52,3 @@ console.log('I am in here.');
 	});
 */
 
-});
