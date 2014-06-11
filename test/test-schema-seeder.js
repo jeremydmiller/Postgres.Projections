8abfbd52cf2761bf7abc118c325c622a7b6638e0
jeremydmiller <jeremydmiller@yahoo.com>
2014-06-11 14:49:32 -0500

    able to generate DDL for projection specific tables

--- test/test-schema-seeder.js
+++ test/test-schema-seeder.js
@@ -3,18 +3,23 @@ var expect = require('chai').expect;
 var seeder = require('../lib/seeder');
 var _ = require('underscore-node');
 var connection = 'postgres://jeremill:@localhost/projections';
+var projectionFolder = __dirname + '/projections';
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
 
 describe('The Database Seeder', function(){
-	//seeder.seedAll({
-	//	connection: connection
-	//});
-
 	it('Should be able to seed the basic database schema', function(done){
 		seeder.seedAll({connection: connection})
 			.then(function(result){
 
-				expect(result.tables).to.deep.equal(['pge_events', 'pge_modules', 'pge_projection_definitions', 'pge_projections', 'pge_streams']);
-				
+				expect(result.tables).to.include('pge_events');
+				expect(result.tables).to.include('pge_modules');
+				expect(result.tables).to.include('pge_projection_definitions');
+				expect(result.tables).to.include('pge_projections');
+				expect(result.tables).to.include('pge_streams');
+
 				expect(result.modules).to.deep.equal(['eventstore', 'persistor', 'pg-events']);
 
 				done();
@@ -26,13 +31,24 @@ describe('The Database Seeder', function(){
 
 	it('should be able to load all the projections from a folder path', function(){
 		var projector = require('../lib/pg-events');
-		
-
-		var files = projector.loadProjectionsFromFolder(__dirname + '/projections');
+		var files = projector.loadProjectionsFromFolder(projectionFolder);
 
 		expect(projector.activeProjectionNames()).to.deep.equal(['Arrival', 'Party', 'Traveled']);
 	});
 
+	it('should be able to generate all the DDL for projection tables', function(){
+		var DDL = seeder.generateDDL({projection_folder: projectionFolder});
+
+		console.log(DDL);
+
+		expect(DDL).to.include('DROP TABLE IF EXISTS pge_projections_Arrival CASCADE;');
+		expect(DDL).to.include('CREATE TABLE pge_projections_Arrival (');
+		expect(DDL).to.include('DROP TABLE IF EXISTS pge_projections_Party CASCADE;');
+		expect(DDL).to.include('CREATE TABLE pge_projections_Party (');
+
+		// shouldn't be adding a new table for an aggregate
+		expect(DDL).to.not.include('Traveled');
+	});
 });
 
 
