67de1d2ce63db35151acbaca71d55c2e793dbff9
jeremydmiller <jeremydmiller@yahoo.com>
2014-06-17 10:12:06 -0500

    added more overloads to startStream()

--- test/test-append-events.js
+++ test/test-append-events.js
@@ -29,6 +29,7 @@ describe('EventStore End to End', function(done){
 
 		return client.startStream(message)
 			.then(function(result){
+				expect(result.id).to.not.be.a('null');
 				return client.fetchStream(result.id);
 			})
 			.then(function(result){
@@ -39,6 +40,24 @@ describe('EventStore End to End', function(done){
 				done(err);
 			});
 	});
+
+	it('should be able to capture an event for a new stream to the database with the simpler usage', function(done){
+		var evt = {location: "Emond's Field", $id: uuid.v4(), $type: 'QuestStarted'};
+
+		return client.startStream('Quest', evt)
+			.then(function(result){
+				expect(result.id).to.not.be.a('null');
+				return client.fetchStream(result.id);
+			})
+			.then(function(result){
+				expect(result[0]).to.deep.equal(evt);
+				done();
+			})
+			.error(function(err){
+				done(err);
+			});
+	});
+
 
 	it('should be able to capture an event for a new stream to the database without an event id', function(done){
 		var message = {
@@ -64,6 +83,7 @@ describe('EventStore End to End', function(done){
 	});
 
 
+
 	it('should append an event to an existing stream to the database', function(done){
 		var message = {
 			streamType: 'Quest',
@@ -82,6 +102,40 @@ describe('EventStore End to End', function(done){
 					id: result.id,
 					data: evt
 				})
+				.then(function(version){
+				// 2nd event in the stream
+					expect(version).to.equal(2);
+					return client.fetchStream(result.id);
+				});
+			})
+			.then(function(stream){
+				delete stream[1].$id;
+
+				expect(stream[1]).to.deep.equal(evt);
+
+				done();
+			})
+			.error(function(err){
+				done(err);
+			});
+	});
+
+
+	it('should append an event to an existing stream to the database with the overload', function(done){
+		var message = {
+			streamType: 'Quest',
+			data: {location: "Emond's Field", $id: uuid.v4(), $type: 'QuestStarted'}
+		};
+
+		var evt = {
+			$type: 'TownReached',
+			location: 'Baerlon',
+			traveled: 5
+		};
+
+		return client.startStream(message)
+			.then(function(result){
+				return client.append(result.id, evt)
 				.then(function(version){
 				// 2nd event in the stream
 					expect(version).to.equal(2);
