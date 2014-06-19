f208287d86bab95711f9626657551aa9b989ce50
jeremydmiller <jeremydmiller@yahoo.com>
2014-06-19 09:30:01 -0500

    Deleting the old sql folder from the spike

--- sql/build.sql
+++
@@ -1,7 +0,0 @@
-\i schema.sql
-\i projections.sql
-\i append_event.sql
-\i load_stream.sql
-\i init.sql
-
-select initialize_projections();
--- sql/init.sql
+++
@@ -1,7 +0,0 @@
---CREATE LANGUAGE PLV8;
-
-CREATE EXTENSION "uuid-ossp";
-
-SET plv8.start_proc = plv8_projections_init;
-
-
